51a3d30ae60ddf804ff140b62b47b08075a8abd9
Ashish Shenoy <ashish1shenoy@gmail.com>
2019-09-10 17:02:08 +0530

    Display keypresses

--- kedit.c
+++ kedit.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -14,13 +16,21 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
     return 0;
 }
