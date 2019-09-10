622c966179342161f3f64c3dfa46bb951af76229
Ashish Shenoy <ashish1shenoy@gmail.com>
2019-09-10 20:51:26 +0530

    Remove option to navigate using WASD keys

--- kedit.c
+++ kedit.c
@@ -15,10 +15,10 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-    ARROW_LEFT = 'a',
-    ARROW_RIGHT = 'd',
-    ARROW_UP = 'w',
-    ARROW_DOWN = 's'
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
 };
 
 /*** data ***/
@@ -61,7 +61,7 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -196,7 +196,7 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_LEFT:
             E.cx--;
@@ -216,7 +216,7 @@ void editorMoveCursor(char key) {
 }
 
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c) {
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
