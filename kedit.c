0f550d638070f5cefbb1a8a486d1c886496e98cf
Ashish Shenoy <ashish1shenoy@gmail.com>
2019-09-10 18:06:03 +0530

    Add vim-like tildes after end of file

--- kedit.c
+++ kedit.c
@@ -55,8 +55,18 @@ char editorReadKey() {
 
 /*** output ***/
 
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+
+    editorDrawRows();
     write(STDOUT_FILENO, "\x1b[1;1H", 6);
 }
 
