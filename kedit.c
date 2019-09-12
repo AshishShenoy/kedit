cab4667d71b9a2ecec84519d49ab18bbf9e04aa8
Ashish Shenoy <ashish1shenoy@gmail.com>
2019-09-12 23:43:19 +0530

    Add ability to insert characters

--- kedit.c
+++ kedit.c
@@ -223,6 +223,25 @@ void editorAppendRow(char *s, size_t len) {
     E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -473,6 +492,7 @@ void editorProcessKeypress() {
             break;
         
         default:
+            editorInsertChar(c);
             break;
     }
 }
